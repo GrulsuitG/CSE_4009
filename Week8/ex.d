2f2c746b322019f31d491e5ef3a66ae661827ffc
2016025205 <2016025205@hanyang.ac.kr>
2020-11-06 01:03:39 +0900

    week8 assignment

---
+++ Week8/ex.d
@@ -0,0 +1,10 @@
+
+ex.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	68 92 19 40 00       	pushq  $0x401992
+   5:	bf b8 f9 60 55       	mov    $0x5560f9b8,%edi
+   a:	c3                   	retq   
---
+++ Week8/ex.s
@@ -0,0 +1,3 @@
+pushq $0x401992
+mov $0x5560f9b8, %edi
+ret
